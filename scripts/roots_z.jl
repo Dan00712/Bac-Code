12e7b1fc0d1011e6eeda945cf6bcd40b4008fbbf
Daniel 'Percy' Steinmaurer <dan.steinmaurer@gmail.com>
2025-10-28 17:52:48 +0100

    redo roots_z.jl
    currently WIP

--- scripts/roots_z.jl
+++ scripts/roots_z.jl
@@ -3,11 +3,9 @@ using DrWatson
 using LinearAlgebra
 using Logging
 
-
 using ProgressBars
 
-using ForwardDiff
-∂ = ForwardDiff.derivative
+using Roots
 
 using Plots
 plotlyjs()
@@ -21,92 +19,46 @@ using SingleCavity.Laser
 using SingleCavity.Newton
 
 
-# We can optimize the method used to a 1d root finding problem
-# since it can be analytically shown that x=y=0 is a requirement we only need to do this for a single function
-# but for checking the full hamiltonian is needed, so this will only be computed on a as needed basis
-
-# to determine guesss for roots we will use the method of creating samples and checking for sign changes
-# this will miss any zero positions where the root only touches zero
-
 global_logger(ConsoleLogger(Info))
 
-function δc(x,y,z, Δω)
-    Δω - α/ħ * Ec(x,y,z, Δω)^2
-end
-
-function γ(x,y,z, Δω)
-    α/ħ * Ec(x,y,z, Δω)/(δc(x,y,z, Δω)^2 + κ^2/4)
-end
-
-function Hs(x, y, z, Δω)
-    -abs2(Et(x, y, z)) +
-    abs2(αeq_c(x, y, z, Δω)*Ec(x, y, z, Δω)) +
-    2*real(Et(x, y, z) * αeq_c(x, y, z, Δω) * Ec(x, y, z, Δω))
-end
-
-# Hessian of the Hamiltonian
-function HHs(x,y,z, Δω)
-    ForwardDiff.hessian(r->Hs(r..., Δω), [x, y, z])
-end
-
 # function to minimize
-function L(z, Δω)
-    γδ = γ(0,0,z, Δω) * δc(0,0,z, Δω)
+function L(z, Δω, κ)
+    γδ = γ(0,0,z, Δω, κ) * δc(0,0,z, Δω)
 
     z * (1+γδ)*(1/zR^2 + 2/Wc^2 * γδ) +
-        γ(0,0, z, Δω) * κ/2 * (2*z/Wc^2 + 1/zR - k0)
+        γ(0,0, z, Δω, κ) * κ/2 * (2*z/Wc^2 + 1/zR - k0)
 end
 
-# Δω = 0:2ω0
-Ωs = 10 .^(-2:12/200:10)
-Zs = (-zR:2zR/1000:zR) |> x->BigFloat.(x)
-#Zs = range(-zR/5, zR/5; length=200)
-@debug "" Zs length(Zs)
 
-function get_roots(Δω; atol=1e-10)
-    f(z) = L(z, Δω)
+Ω = range(start=0, stop=10e6, length=200)
+Z = range(start=-5zR, stop=5zR, length=800)
+const κ = 2π * 1.06e6 /1e2
 
-    guesses = get_guesses(f, Zs)
 
-    zmin = BigFloat[]
-    for guess in guesses
-        x, convergent = newton_1d(guess, f, atol=atol)
-        if convergent
-            push!(
-                  zmin,
-                  x
-                 )
-        end
-    end
+function get_zeros(f)
+    zmin = unique(find_zero(f, z) for z in Z)
 
     zmin
 end
 
-ωs    = Float64[]; #BigFloat[]
-zmins = Float64[]; #BigFloat[]
 
-#@info "Δω = 0" get_roots(0)
-
-
-for  ω in ProgressBar(Ωs)
-    zmins_ = get_roots(ω, atol=1e-30)
-    #@info "" zmins_
-
-    append!(zmins, zmins_)
-    append!(ωs, [ω for _ in zmins_])
+zmins = []
+ωs = []
+for ω in Ω
+    newmins = get_zeros(z->L(z, ω, κ))
+    append!(zmins, newmins)
+    append!(ωs, [ω for _ in newmins])
 end
+
 p = plot(;
-         #ylims=(-1, 1)./1e32,
-         xlabel="Δω/[s^-1]",
-         ylabel="z/[m]",
-         xaxis=:log,
-        )
+)
 
 scatter!(p, ωs, zmins)
-#plot!(p, Zs, L.(Zs,[0]))
-#plot!(p, Zs, (z->∂(x-> Hs(0,0,x, 0), z)).(Zs))
-savefig(p, plotsdir("full_z.N$(size(Zs))-$(now_nodots()).svg"))
 
+savefig(p, plotsdir("full_z-$(now_nodots()).svg"))
 gui(p)
 
-
+if !Base.isinteractive()
+    println("hit enter to close")
+    readline()
+end
