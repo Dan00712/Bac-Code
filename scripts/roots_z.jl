12290a499703dfcfcc3b0093e78f4b2944fc1e89
Daniel 'Percy' Steinmaurer <dan.steinmaurer@gmail.com>
2025-10-30 18:04:50 +0100

    supplemental changes in debugging

--- scripts/roots_z.jl
+++ scripts/roots_z.jl
@@ -23,13 +23,12 @@ global_logger(ConsoleLogger(Info))
 
 # function to minimize
 function L(z, Δω, κ)
-    ∂zHs(0,0, z, Δω, κ)
+    ∂zHs(0, 0, z, Δω, κ)
 end
 
-
 Ω = range(start=0, stop=10e6, length=100)
-Z = range(start=-5zR, stop=5zR, length=200)
-const κ = 2π * 1.06e6 /1e1
+Z = range(start=-100zR, stop=100zR, length=35) |> x-> BigFloat.(x)
+const κ = 2π * 1.06e6 /1e2
 
 
 function get_zeros(f)
@@ -41,7 +40,7 @@ end
 
 zmins = []
 ωs = []
-for ω in Ω
+for ω in ProgressBar(Ω)
     newmins = get_zeros(z->L(z, ω, κ))
     append!(zmins, newmins)
     append!(ωs, [ω for _ in newmins])
