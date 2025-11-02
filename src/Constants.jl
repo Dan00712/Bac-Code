c813ec98c68b0732c0886b9ce1cc092374028844
Daniel Steinmaurer <dan.steinmaurer@gmail.com>
2025-11-02 16:05:18 +0100

    make consistent with previous simulations

--- src/Constants.jl
+++ src/Constants.jl
@@ -6,12 +6,12 @@ export ħ, ϵ0, c, λ0, k0, ω0, Pt, Wt, Ax, Ay, E0, zR, ρ, ϵ, α, Wc, Lc, Vc,
 # using SI Units
 const ħ = 6.626e-34       # Js
 const ϵ0 = 8.854e-12      # F/m
-const c = 3e+8            # m/s
+const c = 2.991e+8            # m/s
 
 
 # Tweezer
 
-const λ0 = 1.55e-6        # m = 1.66μm
+const λ0 = 1550e-9        # m = 1.66μm
 const k0 = 2π/λ0
 const ω0 = c*k0
 
@@ -21,20 +21,23 @@ const Wt = 1e-6           # m
 const Ax = 1
 const Ay = 1
 
-const zR = 1/2 * k0 * Wt^2
+#const zR = 1/2 * k0 * Wt^2
+const zR = π * Wt^2/λ0
 
 
 const E0 = sqrt(4*Pt / π / ϵ0 / c / Wt^2 / Ax^2 / Ay)
 
 # SiO₂
-const ρ = 2200               # kg/m^3
+const ρ = 2200              # kg/m^3
 const ϵ = 2.07
-const α = (ϵ-1)ϵ/ρ
+const R = 100e-9            # m
+const V = 4/3 *π*R^2
+const α = 3*V*ϵ0 * (ϵ-1)/(ϵ+2)
 
 
 # Cavity
-const Wc = 48e-6        # m
-const Lc = 6.46e-3      # m
+const Wc = 20e-6        # m
+const Lc = 19.8e-3      # m
 # const E0c =           # function of \omega_c
 # keep ω_0 = const
 const Vc = π*Wc^2*Lc/4
