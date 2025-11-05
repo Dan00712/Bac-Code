eb2fe89bb49cb25dbadff501c63c1347b8f94e4e
Daniel 'Percy' Steinmaurer <dan.steinmaurer@gmail.com>
2025-11-05 15:26:55 +0100

    correct false volume

--- src/Constants.jl
+++ src/Constants.jl
@@ -4,7 +4,7 @@ export ħ, ϵ0, c, λ0, k0, ω0, Pt, Wt, Ax, Ay, E0, zR, ρ, ϵ, α, Wc, Lc, Vc,
 
 
 # using SI Units
-const ħ = 6.626e-34       # Js
+const ħ = 6.626e-34/2π       # Js
 const ϵ0 = 8.854e-12      # F/m
 const c = 2.991e+8            # m/s
 
@@ -31,7 +31,7 @@ const E0 = sqrt(4*Pt / π / ϵ0 / c / Wt^2 / Ax^2 / Ay)
 const ρ = 2200              # kg/m^3
 const ϵ = 2.07
 const R = 100e-9            # m
-const V = 4/3 *π*R^2
+const V = 4/3 *π*R^3
 const α = 3*V*ϵ0 * (ϵ-1)/(ϵ+2)
 
 
